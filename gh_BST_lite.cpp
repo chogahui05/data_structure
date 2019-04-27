0b346f103d3d17132460079554692c5bd80f2f7d
chogahui05 <chokw336@gmail.com>
2019-04-27 22:02:16 +0900

    Update gh_BST_lite.cpp

--- gh_BST_lite.cpp
+++ gh_BST_lite.cpp
@@ -29,57 +29,57 @@ public:
     	add(&root,x);
     }
     int find_num(int x)
-	  {
-	      node *tt = _find(root,x);
-	      if(tt==NULL)
-	          return 0;
-	      return (tt->is_removed == 0);
-	  }
-	  void remove(int x)
-	  {
-		    if(find_num(x)==0) //if x is not find. ignore remove Query
-		        return;
-		    node *tt = _find(root,x); //have x is exist and tt->is_removed == 0
-		    tt->is_removed = 1; remove_count++;
-		    if(remove_count>=remove_node_bound)
-		    {
-			      //rebuild_tree
-			      rebuild();
-			      remove_count = 0;
-		    }
+    {
+        node *tt = _find(root,x);
+	if(tt==NULL)
+	    return 0;
+	return (tt->is_removed == 0);
+    }
+    void remove(int x)
+    {
+        if(find_num(x)==0) //if x is not find. ignore remove Query
+            return;
+        node *tt = _find(root,x); //have x is exist and tt->is_removed == 0
+        tt->is_removed = 1; remove_count++;
+        if(remove_count>=remove_node_bound)
+        {
+	    //rebuild_tree
+	    rebuild();
+	    remove_count = 0;
+        }
     }
 private:
     gh_init()
     {
         root = NULL;
-    	  remove_count = 0;
+    	remove_count = 0;
     }
-	  void rebuild()
-	  {
-		    new_root = NULL;
+    void rebuild()
+    {
+        new_root = NULL;
 		
-		    //traverse.
-		    do_dfs(root,&new_root);
-		    remove_all(root);
-		    root = new_root;
-	  }
-	  void remove_all(node *cur)
-	  {
-		    if(cur==NULL)
-		        return;
-		    remove_all(cur->left);
-		    remove_all(cur->right);
-		    delete cur;
-	  }
-	  void do_dfs(node *cur,node **NEW_ROOT)
-	  {
-		    if(cur==NULL)
-		        return;
-		    if(cur->is_removed==0)
-			      add(NEW_ROOT,cur->num);
-		    do_dfs(cur->left,NEW_ROOT);
-		    do_dfs(cur->right,NEW_ROOT);
-	  }
+	//traverse.
+	do_dfs(root,&new_root);
+	remove_all(root);
+	root = new_root;
+    }
+    void remove_all(node *cur)
+    {
+        if(cur==NULL)
+            return;
+        remove_all(cur->left);
+        remove_all(cur->right);
+        delete cur;
+    }
+    void do_dfs(node *cur,node **NEW_ROOT)
+    {
+        if(cur==NULL)
+	    return;
+        if(cur->is_removed==0)
+	    add(NEW_ROOT,cur->num);
+        do_dfs(cur->left,NEW_ROOT);
+        do_dfs(cur->right,NEW_ROOT);
+    }
 	  void add(node **root,int x)
 	  {
 		    node *cur,*tt;
@@ -110,27 +110,27 @@ private:
 		    //ok. connect_node.
 		    _connect(cur,x);
     }
-	  void _connect(node *tar,int x)
-	  {
-		    if(x<(tar->num))
-		        tar->left = make_node(x);
-		    else
-		        tar->right = make_node(x);
-	  }
+    void _connect(node *tar,int x)
+    {
+        if(x<(tar->num))
+	    tar->left = make_node(x);
+        else
+            tar->right = make_node(x);
+    }
     node *_find(node *root,int x)
     {
         node *cur = root;
-    	  while(cur!=NULL)
-    	  {
-    		    int key = cur->num;
-    		    if(x<key)
-    		        cur = cur->left;
-    		    else if(x>key)
-    		        cur = cur->right;
-    		    else
-    		        return cur;
-		    }
-		    return cur;
+    	while(cur!=NULL)
+        {
+    	    int key = cur->num;
+    	    if(x<key)
+    		cur = cur->left;
+            else if(x>key)
+    		cur = cur->right;
+    	    else
+    	        return cur;
+	}
+        return cur;
     }
     node *make_node(int x)
     {
@@ -139,7 +139,7 @@ private:
         _new->is_removed = 0;
         _new->left = _new->right = NULL;
         return _new;
-	  }
+    }
 };
 int main(void)
 {
