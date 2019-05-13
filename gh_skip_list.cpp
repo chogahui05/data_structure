2cb220fcc222831f2b6aaa0a2de29b6eaeec6225
chogahui05 <chokw336@gmail.com>
2019-05-13 18:00:26 +0900

    Update gh_skip_list.cpp

--- gh_skip_list.cpp
+++ gh_skip_list.cpp
@@ -8,7 +8,8 @@ class gh_skiplist
         int sz;
         struct node **go;
         int *jump;
-	};
+    };
+    typedef struct node node;
 private:
     int add_num;
     node *root;
@@ -28,69 +29,68 @@ public:
     	int temp_jump[20]={0};
     	int tj = 1;
     	init_node(__new,x,__bit);
-        //printf("bit = %d\n",__bit);
     	prev[19] = root;
-		for(int i=19;i>=0;i--)
+	for(int i=19;i>=0;i--)
     	{
-    		//node position init.
-    		if(i!=19)
-    		    prev[i] = prev[i+1];
-    		while((prev[i]->go[i]!=NULL)&&(prev[i]->go[i]->value<x))
-    		{
-    			temp_jump[i] = temp_jump[i] + (prev[i]->jump[i]);
-    		    prev[i] = (prev[i]->go[i]);
-    		}
-		}
-		for(int i=19;i>=__bit;i--)
-		    (prev[i]->jump[i]) = (prev[i]->jump[i])+1;
+    	    //node position init.
+            if(i!=19)
+    	        prev[i] = prev[i+1];
+    	    while((prev[i]->go[i]!=NULL)&&(prev[i]->go[i]->value<x))
+    	    {
+                temp_jump[i] = temp_jump[i] + (prev[i]->jump[i]);
+    		prev[i] = (prev[i]->go[i]);
+    	    }
+	}
+        for(int i=19;i>=__bit;i--)
+            (prev[i]->jump[i]) = (prev[i]->jump[i])+1;
 		
-		for(int i=0;i<__bit;i++)
-		{
-			int total_jump = (prev[i]->jump[i])+1;
-			prev[i]->jump[i] = tj;
-			__new->jump[i] = total_jump-tj;
-			tj = tj + temp_jump[i];
-		}
+        for(int i=0;i<__bit;i++)
+        {
+            int total_jump = (prev[i]->jump[i])+1;
+            prev[i]->jump[i] = tj;
+            __new->jump[i] = total_jump-tj;
+            tj = tj + temp_jump[i];
+        }
     	for(int i=0;i<__bit;i++)
     	{
-    		//link two
-    		__new->go[i] = prev[i]->go[i];
-			prev[i]->go[i] = __new;
-		}
-		//how to select add_num?
-		add_num = next(); add_num = (add_num&1048575);
-	}
-	void remove(int x)
-	{
-		node *prev[20];
+            //link two
+            __new->go[i] = prev[i]->go[i];
+            prev[i]->go[i] = __new;
+        }
+        //how to select add_num?
+        add_num = next(); add_num = (add_num&1048575);
+    }
+    void remove(int x)
+    {
+        node *prev[20];
     	node *cur[20];
     	node *current_node;
     	int sz;
     	prev[19] = root;
-		for(int i=19;i>=0;i--)
+        for(int i=19;i>=0;i--)
     	{
-    		//node position init.
-    		if(i!=19)
-    		    prev[i] = prev[i+1];
-    		while((prev[i]->go[i]!=NULL)&&(prev[i]->go[i]->value<x))
-    		    prev[i] = (prev[i]->go[i]);
-		}
-		current_node = prev[0]->go[0];
-		sz = (current_node->sz);
-		if(current_node==NULL||current_node->value!=x) //not find x
-		    return;
-		for(int i=0;i<sz;i++)
-		{
-			int aa = prev[i]->jump[i];
-			int bb = current_node->jump[i]; //current_node == prev[0]->go[0]
-			//jumping distance update
-			prev[i]->jump[i] = aa+bb-1;
-			prev[i]->go[i] = current_node->go[i];
-		}
-		free(current_node);
-		for(int i=sz;i<20;i++)
-		    prev[i]->jump[i] = (prev[i]->jump[i])-1;
-	}
+            //node position init.
+            if(i!=19)
+                prev[i] = prev[i+1];
+            while((prev[i]->go[i]!=NULL)&&(prev[i]->go[i]->value<x))
+                prev[i] = (prev[i]->go[i]);
+        }
+        current_node = prev[0]->go[0];
+        sz = (current_node->sz);
+        if(current_node==NULL||current_node->value!=x) //not find x
+            return;
+        for(int i=0;i<sz;i++)
+        {
+            int aa = prev[i]->jump[i];
+            int bb = current_node->jump[i]; //current_node == prev[0]->go[0]
+            //jumping distance update
+            prev[i]->jump[i] = aa+bb-1;
+            prev[i]->go[i] = current_node->go[i];
+        }
+        free(current_node);
+        for(int i=sz;i<20;i++)
+            prev[i]->jump[i] = (prev[i]->jump[i])-1;
+    }
 	int kth(int x)
 	{
 		int ret = 0;
